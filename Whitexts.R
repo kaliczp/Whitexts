a26dbdca6d8e94fa9b9a811cb2a5fa5fcae555ae
Péter Kalicz <kaliczp@gmail.com>
2018-10-26 21:21:30 +0200

    Slope, mint vektor és a napok közötti változás meghatározása, összegzése

--- Whitexts.R
+++ Whitexts.R
@@ -17,9 +17,18 @@ White <- function(x, methode = "sampling", Sy = 0.1) {
     slope <- period.apply(diff.x, fourhour.idx, mean)
     valid.idx <- seq(2, length(slope), by = 6)
     slope <- slope[valid.idx]
+    slope.end <- as.numeric(coredata(slope)*48)
+    slope.end <- slope.end[-length(slope.end)]
     ## 2) Az eredeti idősor 0:00 különbsége.
     ## 3) Mostani 00:00 órából van kivonva az előző merre változik a trend?
+    day.end <- endpoints(x, on="days")
+    day.begin <- day.end + 1
+    values.at.midnight <- x[day.begin[-length(day.begin)]]
+    daily.diffs <- diff.xts(values.at.midnight)
+    daily.at.place <- values.at.midnight - as.numeric(coredata(daily.diffs))
+    daily.at.place <- na.omit(daily.at.place)
     ## 4) Összeadni időlépés mennyi változik? idő szerint?
+    top.slope <- daily.at.place + slope.end
     ## 5) Sy elődnél lesz Sy=0.1/2 Meyboom a pórustér 50 % ürül csak le!
-    slope
+    top.slope - values.at.midnight
 }
