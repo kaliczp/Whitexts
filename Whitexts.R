1b922831ef945f6d1d726c239e81913be26b836d
Péter Kalicz <kaliczp@gmail.com>
2018-11-06 21:02:28 +0100

    Slope explanation

--- Whitexts.R
+++ Whitexts.R
@@ -16,8 +16,12 @@ White <- function(x, methode = "sampling", Sy = 0.1) {
     fourhour.idx <- endpoints(diff.x, on = "hours", k = 4)
     slope <- period.apply(diff.x, fourhour.idx, mean)
     valid.idx <- seq(1, length(slope), by = 6)
-    slope.ok <- slope[valid.idx]
-    slope.end <- as.numeric(coredata(slope.ok)*48)
+    ## Iránytangens órás
+    slope.ok <- slope[valid.idx]*2
+    slope.core <- coredata(slope.ok)
+    slope.hourly <- xts(slope.core, round.POSIXt(index(slope.ok), unit="day"))
+    ## Napi vonal
+    slope.end <- as.numeric(slope.core * 24)
     slope.end <- slope.end[-length(slope.end)]
     ## 2) Az eredeti idősor 0:00 különbsége.
     ## 3) Mostani 00:00 órából van kivonva az előző merre változik a trend?
@@ -34,7 +38,12 @@ White <- function(x, methode = "sampling", Sy = 0.1) {
     ET <- (top.slope - values.at.midnight)*Sy*1000/2
     colnames(ET) <- "CalculatedET"
     ET <- round(ET,4)
-    list(ori.gw = x, results = merge.xts(daily.at.place,values.at.midnight,top.slope,ET))
+    list(ori.gw = x, results = merge.xts(slope.hourly,
+                                         daily.at.place,
+                                         values.at.midnight,
+                                         top.slope,
+                                         ET)
+         )
 }
 
 plot.White <- function(x){
